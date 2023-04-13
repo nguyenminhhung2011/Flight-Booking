c478281442409f14f59faf8d2d3d16e3a0039b1e
hoangankin2211 <truonghuynhduchoang@gmail.com>
2023-04-13 12:14:13 +0700

    add some component for login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flight_booking/presentations/login/views/login_screen.dart';
 import 'package:flight_booking/presentations/routes/main_routes.dart';
 import 'package:flight_booking/presentations/routes/routes.dart';
 import 'package:flutter/material.dart';
@@ -5,9 +6,7 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'generated/l10n.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -17,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Flight booking',
       debugShowCheckedModeBanner: false,
       onGenerateRoute: MainRoutes.getRoute,
-      initialRoute: Routes.dashboard,
+      // initialRoute: Routes.dashboard,
       localizationsDelegates: const [
         S.delegate,
         GlobalMaterialLocalizations.delegate,
@@ -28,7 +27,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.light,
-      home: const SizedBox(),
+      home: Scaffold(body: LoginScreen()),
     );
   }
 }
