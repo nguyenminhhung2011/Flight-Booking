4439c6261d983f29bc27ea65268705407bb376d5
hoangankin2211 <truonghuynhduchoang@gmail.com>
2023-04-13 15:36:34 +0700

    finished splash,login,forget password (not clean code yet)

--- lib/presentations/routes/routes.dart
+++ lib/presentations/routes/routes.dart
@@ -2,4 +2,5 @@ class Routes {
   static const String home = '/home';
   static const String dashboard = '/dashboard';
   static const String login = "/login";
+  static const String splash = "/splash";
 }
