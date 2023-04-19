157aec5576d162c520fdf82679ba9a97bb3bc0bc
Nguyễn Minh Hưng <90996598+minhunsocute@users.noreply.github.com>
2023-04-19 12:24:39 +0700

    ☀️Feature ticet fastview screen

--- lib/app_coordinator.dart
+++ lib/app_coordinator.dart
@@ -1,4 +1,3 @@
-import 'package:flight_booking/core/components/enum/view_enum.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
---
+++ lib/presentations/list_ticket/views/widgets/ticket_details_widget.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class TicketDetailsWidget extends StatelessWidget {
+  final String firstTitle;
+  final String firstDesc;
+  final String secondTitle;
+  final String secondDesc;
+  const TicketDetailsWidget({
+    super.key,
+    required this.firstTitle,
+    required this.firstDesc,
+    required this.secondTitle,
+    required this.secondDesc,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle titleStyle =
+        Theme.of(context).textTheme.titleMedium!.copyWith(color: Colors.grey);
+
+    final TextStyle? descStyle = Theme.of(context).textTheme.titleMedium;
+
+    return ListTile(
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            firstTitle,
+            maxLines: 1,
+            style: titleStyle,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 4.0),
+            child: Text(
+              firstDesc,
+              maxLines: 1,
+              style: descStyle,
+            ),
+          )
+        ],
+      ),
+      trailing: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            secondTitle,
+            maxLines: 1,
+            style: titleStyle,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 4.0),
+            child: Text(
+              secondDesc,
+              maxLines: 1,
+              style: descStyle,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
