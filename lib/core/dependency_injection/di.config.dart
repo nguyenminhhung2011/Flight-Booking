f2432f341f89b5c61e13480557e7493be3f7f632
Nguyễn Minh Hưng <90996598+minhunsocute@users.noreply.github.com>
2023-04-25 00:02:12 +0700

    ☀️⬆️ Update feature list ticket screen UI

--- lib/core/dependency_injection/di.config.dart
+++ lib/core/dependency_injection/di.config.dart
@@ -12,15 +12,17 @@
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import '../../data/datasource/remote/rest_api/rest_api.dart' as _i6;
-import '../../data/repositories/list_flight_repository_impl.dart' as _i8;
-import '../../data/repositories/list_tic_repository_impl.dart' as _i10;
-import '../../domain/repositories/list_flight_repository.dart' as _i7;
-import '../../domain/repositories/list_tic_repository.dart' as _i9;
-import '../../domain/usecase/list_tic_usecase.dart' as _i5;
+import '../../data/datasource/remote/rest_api/rest_api.dart' as _i5;
+import '../../data/repositories/list_flight_repository_impl.dart' as _i7;
+import '../../data/repositories/list_ticket_repository_impl.dart' as _i10;
+import '../../domain/repositories/list_flight_repository.dart' as _i6;
+import '../../domain/repositories/list_ticket_repository.dart' as _i9;
+import '../../domain/usecase/list_flight_usecase.dart' as _i8;
+import '../../domain/usecase/list_ticket_usecase.dart' as _i11;
 import '../../presentations/dashboard/bloc/dashboard_bloc.dart' as _i3;
 import '../../presentations/flight_detail/bloc/flight_detail_bloc.dart' as _i4;
-import '../../presentations/list_flight/bloc/list_flight_bloc.dart' as _i11;
+import '../../presentations/list_flight/bloc/list_flight_bloc.dart' as _i12;
+import '../../presentations/list_ticket/bloc/list_ticket_bloc.dart' as _i13;
 
 // ignore_for_file: unnecessary_lambdas
 // ignore_for_file: lines_longer_than_80_chars
@@ -37,13 +39,18 @@ _i1.GetIt init(
   );
   gh.factory<_i3.DashboardBloc>(() => _i3.DashboardBloc());
   gh.factory<_i4.FlightDetailBloc>(() => _i4.FlightDetailBloc());
-  gh.factory<_i5.ListTicUsecase>(() => _i5.ListTicUsecase());
-  gh.factory<_i6.RestApi>(() => _i6.RestApi());
-  gh.factory<_i7.ListFlightRepository>(
-      () => _i8.ListFlightRepositoryImpl(gh<_i6.RestApi>()));
-  gh.factory<_i9.ListTicRepository>(
-      () => _i10.ListTicRepositoryImpl(gh<_i6.RestApi>()));
-  gh.factory<_i11.ListFlightBloc>(
-      () => _i11.ListFlightBloc(gh<_i7.ListFlightRepository>()));
+  gh.factory<_i5.RestApi>(() => _i5.RestApi());
+  gh.factory<_i6.ListFlightRepository>(
+      () => _i7.ListFlightRepositoryImpl(gh<_i5.RestApi>()));
+  gh.factory<_i8.ListFlightsUsecase>(
+      () => _i8.ListFlightsUsecase(gh<_i6.ListFlightRepository>()));
+  gh.factory<_i9.ListTicketRepository>(
+      () => _i10.ListTicketRepositoryImpl(gh<_i5.RestApi>()));
+  gh.factory<_i11.ListTicketUsecase>(
+      () => _i11.ListTicketUsecase(gh<_i9.ListTicketRepository>()));
+  gh.factory<_i12.ListFlightBloc>(
+      () => _i12.ListFlightBloc(gh<_i8.ListFlightsUsecase>()));
+  gh.factory<_i13.ListTicketBloc>(
+      () => _i13.ListTicketBloc(gh<_i11.ListTicketUsecase>()));
   return getIt;
 }
--- lib/presentations/list_ticket/views/list_ticket_screen.dart
+++ lib/presentations/list_ticket/views/list_ticket_screen.dart
@@ -1,11 +1,15 @@
+import 'package:flight_booking/core/components/enum/item_view_enum.dart';
+import 'package:flight_booking/presentations/list_ticket/bloc/list_ticket_bloc.dart';
 import 'package:flight_booking/presentations/list_ticket/views/widgets/column_tic_view.dart';
 import 'package:flight_booking/presentations/list_ticket/views/widgets/position_dialog.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../core/components/enum/tic_type_enum.dart';
 import '../../../generated/l10n.dart';
 import '../../flight_detail/views/widgets/icon_button.dart';
+import '../../flight_detail/views/widgets/tic_column_list_view.dart';
 
 class ListTicketScreen extends StatefulWidget {
   const ListTicketScreen({
@@ -17,12 +21,15 @@ class ListTicketScreen extends StatefulWidget {
 }
 
 class _ListTicketScreenState extends State<ListTicketScreen> {
+  ListTicketBloc get _bloc => BlocProvider.of<ListTicketBloc>(context);
+
   late final _textController;
 
   @override
   void initState() {
     super.initState();
     _textController = TextEditingController();
+    _bloc.add(const ListTicketEvent.started());
   }
 
   void _showPositonDia(positonClic, position, type) {
@@ -44,127 +51,172 @@ class _ListTicketScreenState extends State<ListTicketScreen> {
     );
   }
 
+  void _listenStateChange(_, state) {
+    state.whenOrNull();
+  }
+
+  void _changeItemView(ItemViewEnum itemView) {
+    _bloc.add(ListTicketEvent.changeTicketItemView(itemView));
+  }
+
   @override
   Widget build(BuildContext context) {
     final widthField = MediaQuery.of(context).size.width;
 
-    return Scaffold(
-      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      body: Column(
-        children: [
-          ListTile(
-            title: Text(
-              S.of(context).listTicket,
-              style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                    fontWeight: FontWeight.bold,
-                  ),
-              maxLines: 1,
-            ),
-            trailing: SizedBox(
-              width: widthField * 0.4,
-              child: CupertinoSearchTextField(
-                padding: const EdgeInsets.all(10.0),
-                controller: _textController,
-                enabled: true,
-                style: Theme.of(context).textTheme.titleMedium,
-                onChanged: (value) {},
-                onSubmitted: (value) {},
-                backgroundColor:
-                    Theme.of(context).primaryColor.withOpacity(0.05),
-                placeholder: S.of(context).searchAnything,
-              ),
-            ),
-          ),
-          const SizedBox(height: 10.0),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0),
-            child: Row(
-              children: [
-                InkWell(
-                  onTap: () {},
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        '${S.of(context).filter} ',
-                        style: Theme.of(context)
-                            .textTheme
-                            .titleSmall!
-                            .copyWith(fontWeight: FontWeight.bold),
+    return BlocConsumer<ListTicketBloc, ListTicketState>(
+      listener: _listenStateChange,
+      builder: (context, state) {
+        final data = state.data;
+        return Scaffold(
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          body: Column(
+            children: [
+              ListTile(
+                title: Text(
+                  S.of(context).listTicket,
+                  style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                        fontWeight: FontWeight.bold,
                       ),
-                      const Icon(Icons.keyboard_arrow_down_rounded, size: 14.0),
-                    ],
+                  maxLines: 1,
+                ),
+                trailing: SizedBox(
+                  width: widthField * 0.4,
+                  child: CupertinoSearchTextField(
+                    padding: const EdgeInsets.all(10.0),
+                    controller: _textController,
+                    enabled: true,
+                    style: Theme.of(context).textTheme.titleMedium,
+                    onChanged: (value) {},
+                    onSubmitted: (value) {},
+                    backgroundColor:
+                        Theme.of(context).primaryColor.withOpacity(0.05),
+                    placeholder: S.of(context).searchAnything,
                   ),
                 ),
-                const Spacer(),
-                IconButtonCustom(
-                  iconData: Icons.list,
-                  onPress: () {},
-                  isSec: true,
-                ),
-                IconButtonCustom(
-                  iconData: Icons.grid_view_sharp,
-                  onPress: () {},
-                  isSec: false,
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              height: double.infinity,
-              margin: const EdgeInsets.all(10.0),
-              padding: const EdgeInsets.all(10.0),
-              decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor.withOpacity(0.1),
-                borderRadius: BorderRadius.circular(10.0),
               ),
-              child: ListView(
-                children: [
-                  SizedBox(
-                    width: double.infinity,
-                    height: 100,
-                    child: Row(
-                      children: const [
-                        Expanded(
-                          child: FilterCategory(
-                            title: 'Flights',
-                            hint: 'Select Flights',
-                            iconData: Icons.airplane_ticket,
+              const SizedBox(height: 10.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: Row(
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            '${S.of(context).filter} ',
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleSmall!
+                                .copyWith(fontWeight: FontWeight.bold),
                           ),
-                        )
-                      ],
+                          const Icon(Icons.keyboard_arrow_down_rounded,
+                              size: 14.0),
+                        ],
+                      ),
                     ),
-                  ),
-                  ColumnTicView(
-                    onPress: (positonClic, position) => _showPositonDia(
-                        positonClic, position, TicTypeEnum.economyClass),
-                    type: TicTypeEnum.economyClass,
-                  ),
-                  ColumnTicView(
-                    onPress: (positonClic, position) => _showPositonDia(
-                        positonClic, position, TicTypeEnum.businessClass),
-                    type: TicTypeEnum.businessClass,
-                  ),
-                  ColumnTicView(
-                    onPress: (positonClic, position) => _showPositonDia(
-                        positonClic, position, TicTypeEnum.premiumEconomyClass),
-                    type: TicTypeEnum.premiumEconomyClass,
-                  ),
-                  ColumnTicView(
-                    onPress: (positonClic, position) => _showPositonDia(
-                        positonClic, position, TicTypeEnum.firstClass),
-                    type: TicTypeEnum.firstClass,
-                  ),
-                ]
-                    .expand((element) => [element, const SizedBox(height: 5)])
-                    .toList(),
+                    const Spacer(),
+                    IconButtonCustom(
+                      iconData: Icons.list,
+                      onPress: () => _changeItemView(ItemViewEnum.listView),
+                      isSec: data.itemView == ItemViewEnum.listView,
+                    ),
+                    IconButtonCustom(
+                      iconData: Icons.grid_view_sharp,
+                      onPress: () => _changeItemView(ItemViewEnum.gridView),
+                      isSec: data.itemView == ItemViewEnum.gridView,
+                    ),
+                  ],
+                ),
               ),
-            ),
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  margin: const EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor.withOpacity(0.1),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: ListView(
+                    children: [
+                      SizedBox(
+                        width: double.infinity,
+                        height: 100,
+                        child: Row(
+                          children: const [
+                            Expanded(
+                              child: FilterCategory(
+                                title: 'Flights',
+                                hint: 'Select Flights',
+                                iconData: Icons.airplane_ticket,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      data.itemView.isGridView
+                          ? Column(
+                              children: [
+                                ColumnTicView(
+                                  onPress: (positonClic, position) =>
+                                      _showPositonDia(positonClic, position,
+                                          TicTypeEnum.economyClass),
+                                  type: TicTypeEnum.economyClass,
+                                ),
+                                ColumnTicView(
+                                  onPress: (positonClic, position) =>
+                                      _showPositonDia(positonClic, position,
+                                          TicTypeEnum.businessClass),
+                                  type: TicTypeEnum.businessClass,
+                                ),
+                                ColumnTicView(
+                                  onPress: (positonClic, position) =>
+                                      _showPositonDia(positonClic, position,
+                                          TicTypeEnum.premiumEconomyClass),
+                                  type: TicTypeEnum.premiumEconomyClass,
+                                ),
+                                ColumnTicView(
+                                  onPress: (positonClic, position) =>
+                                      _showPositonDia(positonClic, position,
+                                          TicTypeEnum.firstClass),
+                                  type: TicTypeEnum.firstClass,
+                                ),
+                              ],
+                            )
+                          : Column(
+                              children: const [
+                                TicColumnListView(
+                                  ticType: TicTypeEnum.businessClass,
+                                  count: 10,
+                                ),
+                                TicColumnListView(
+                                  ticType: TicTypeEnum.businessClass,
+                                  count: 10,
+                                ),
+                                TicColumnListView(
+                                  ticType: TicTypeEnum.premiumEconomyClass,
+                                  count: 10,
+                                ),
+                                TicColumnListView(
+                                  ticType: TicTypeEnum.firstClass,
+                                  count: 10,
+                                ),
+                              ],
+                            )
+                    ]
+                        .expand(
+                            (element) => [element, const SizedBox(height: 5)])
+                        .toList(),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
