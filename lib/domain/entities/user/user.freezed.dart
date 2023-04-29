0193d20ca393c3ae11c2fa7c03286a89b5535ec9
= <=>
2023-04-29 16:08:51 +0700

    finished 80% settings screen

---
+++ lib/domain/entities/user/user.freezed.dart
@@ -0,0 +1,253 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'user.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$User {
+  String get id => throw _privateConstructorUsedError;
+  String get username => throw _privateConstructorUsedError;
+  String get password => throw _privateConstructorUsedError;
+  String get name => throw _privateConstructorUsedError;
+  DateTime get birthday => throw _privateConstructorUsedError;
+  String get email => throw _privateConstructorUsedError;
+  String get phone => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $UserCopyWith<User> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $UserCopyWith<$Res> {
+  factory $UserCopyWith(User value, $Res Function(User) then) =
+      _$UserCopyWithImpl<$Res, User>;
+  @useResult
+  $Res call(
+      {String id,
+      String username,
+      String password,
+      String name,
+      DateTime birthday,
+      String email,
+      String phone});
+}
+
+/// @nodoc
+class _$UserCopyWithImpl<$Res, $Val extends User>
+    implements $UserCopyWith<$Res> {
+  _$UserCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? username = null,
+    Object? password = null,
+    Object? name = null,
+    Object? birthday = null,
+    Object? email = null,
+    Object? phone = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      username: null == username
+          ? _value.username
+          : username // ignore: cast_nullable_to_non_nullable
+              as String,
+      password: null == password
+          ? _value.password
+          : password // ignore: cast_nullable_to_non_nullable
+              as String,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      birthday: null == birthday
+          ? _value.birthday
+          : birthday // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      email: null == email
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as String,
+      phone: null == phone
+          ? _value.phone
+          : phone // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_UserCopyWith<$Res> implements $UserCopyWith<$Res> {
+  factory _$$_UserCopyWith(_$_User value, $Res Function(_$_User) then) =
+      __$$_UserCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String id,
+      String username,
+      String password,
+      String name,
+      DateTime birthday,
+      String email,
+      String phone});
+}
+
+/// @nodoc
+class __$$_UserCopyWithImpl<$Res> extends _$UserCopyWithImpl<$Res, _$_User>
+    implements _$$_UserCopyWith<$Res> {
+  __$$_UserCopyWithImpl(_$_User _value, $Res Function(_$_User) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? username = null,
+    Object? password = null,
+    Object? name = null,
+    Object? birthday = null,
+    Object? email = null,
+    Object? phone = null,
+  }) {
+    return _then(_$_User(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      username: null == username
+          ? _value.username
+          : username // ignore: cast_nullable_to_non_nullable
+              as String,
+      password: null == password
+          ? _value.password
+          : password // ignore: cast_nullable_to_non_nullable
+              as String,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      birthday: null == birthday
+          ? _value.birthday
+          : birthday // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      email: null == email
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as String,
+      phone: null == phone
+          ? _value.phone
+          : phone // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_User implements _User {
+  const _$_User(
+      {required this.id,
+      required this.username,
+      required this.password,
+      required this.name,
+      required this.birthday,
+      required this.email,
+      required this.phone});
+
+  @override
+  final String id;
+  @override
+  final String username;
+  @override
+  final String password;
+  @override
+  final String name;
+  @override
+  final DateTime birthday;
+  @override
+  final String email;
+  @override
+  final String phone;
+
+  @override
+  String toString() {
+    return 'User(id: $id, username: $username, password: $password, name: $name, birthday: $birthday, email: $email, phone: $phone)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_User &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.username, username) ||
+                other.username == username) &&
+            (identical(other.password, password) ||
+                other.password == password) &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.birthday, birthday) ||
+                other.birthday == birthday) &&
+            (identical(other.email, email) || other.email == email) &&
+            (identical(other.phone, phone) || other.phone == phone));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType, id, username, password, name, birthday, email, phone);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_UserCopyWith<_$_User> get copyWith =>
+      __$$_UserCopyWithImpl<_$_User>(this, _$identity);
+}
+
+abstract class _User implements User {
+  const factory _User(
+      {required final String id,
+      required final String username,
+      required final String password,
+      required final String name,
+      required final DateTime birthday,
+      required final String email,
+      required final String phone}) = _$_User;
+
+  @override
+  String get id;
+  @override
+  String get username;
+  @override
+  String get password;
+  @override
+  String get name;
+  @override
+  DateTime get birthday;
+  @override
+  String get email;
+  @override
+  String get phone;
+  @override
+  @JsonKey(ignore: true)
+  _$$_UserCopyWith<_$_User> get copyWith => throw _privateConstructorUsedError;
+}
---
+++ lib/presentations/settings/views/setting_screen.dart
@@ -0,0 +1,269 @@
+import 'package:flight_booking/core/config/common_ui_config.dart';
+import 'package:flight_booking/generated/l10n.dart';
+import 'package:flight_booking/presentations/settings/widgets/account_setting_tab.dart';
+import 'package:flight_booking/presentations/settings/widgets/general_setting_tab.dart';
+import 'package:flight_booking/presentations/settings/widgets/principle_setting_tab.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../core/components/widgets/label_textfield.dart';
+
+class SettingScreen extends StatefulWidget {
+  const SettingScreen({super.key});
+
+  @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  late final List<Map<String, dynamic>> navigatorBarData = [
+    {
+      "label": S.of(context).generalSetting,
+      "icon": Icons.person,
+    },
+    {
+      "label": S.of(context).accountSetting,
+      "icon": Icons.password,
+    },
+    {
+      "label": S.of(context).ruleSettings,
+      "icon": Icons.rule,
+    },
+  ];
+
+  late final List<Widget> pages = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 0,
+        title: Text(
+          S.of(context).setting,
+          maxLines: 1,
+          style: Theme.of(context)
+              .textTheme
+              .headlineSmall!
+              .copyWith(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              NavigationBar(
+                height: 50,
+                backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                destinations: navigatorBarData
+                    .map(
+                      (destination) => NavigationDestination(
+                        icon: Icon(destination["icon"]),
+                        label: destination['label'],
+                      ),
+                    )
+                    .toList(),
+                selectedIndex: 0,
+                onDestinationSelected: (value) {},
+              ),
+              const SizedBox(height: 15),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        width: 1, color: Theme.of(context).dividerColor),
+                    borderRadius: CommonAppUIConfig.primaryRadiusBorder,
+                  ),
+                  child: PageView(
+                    children: [
+                      PrincipleSettingTab(),
+                      AccountSettingTab(),
+                      GeneralSettingsTab(),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class XL extends StatelessWidget {
+  const XL({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          S.of(context).generalSetting,
+          style: Theme.of(context)
+              .textTheme
+              .headlineSmall
+              ?.copyWith(fontWeight: FontWeight.bold),
+        ),
+        Divider(
+          height: 10,
+          thickness: 0.5,
+          color: Theme.of(context).dividerColor,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 30,
+              backgroundColor: Theme.of(context).cardColor,
+              child: const Icon(
+                Icons.person,
+                size: 30,
+              ),
+            ),
+            const SizedBox(width: 5),
+            TextButton.icon(
+              style: TextButton.styleFrom(
+                backgroundColor: Theme.of(context).cardColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: CommonAppUIConfig.primaryRadiusBorder,
+                ),
+              ),
+              onPressed: () {},
+              icon: const Icon(Icons.upload_file),
+              label: Text(
+                S.of(context).uploadProfileImage,
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+          ],
+        ),
+        Divider(
+          height: 10,
+          thickness: 0.5,
+          color: Theme.of(context).dividerColor,
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: LabelTextField(title: S.of(context).username),
+            ),
+            const SizedBox(width: 50),
+            Expanded(
+              child: LabelTextField(title: S.of(context).name),
+            ),
+          ],
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: LabelTextField(title: S.of(context).username),
+            ),
+            const SizedBox(width: 50),
+            Expanded(
+              child: LabelTextField(title: S.of(context).name),
+            ),
+          ],
+        ),
+        Divider(
+          height: 10,
+          thickness: 0.5,
+          color: Theme.of(context).dividerColor,
+        ),
+        TextButton.icon(
+          onPressed: () {},
+          style: TextButton.styleFrom(
+            backgroundColor:
+                Theme.of(context).buttonTheme.colorScheme?.background,
+          ),
+          icon: const Icon(Icons.check),
+          label: Text(
+            S.of(context).saveChange,
+            style: Theme.of(context).textTheme.bodySmall,
+          ),
+        ),
+        const SizedBox(height: 20),
+        Container(
+          padding: const EdgeInsets.all(25),
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Theme.of(context).dividerColor),
+            borderRadius: CommonAppUIConfig.primaryRadiusBorder,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                S.of(context).emailPhoneDetail,
+                style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              Divider(
+                height: 10,
+                thickness: 0,
+                color: Theme.of(context).dividerColor,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: LabelTextField(
+                      title: S.of(context).email,
+                      isColumn: false,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      backgroundColor: Theme.of(context).cardColor,
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: CommonAppUIConfig.primaryRadiusBorder,
+                        side: BorderSide(
+                            color: Theme.of(context).dividerColor, width: 0.5),
+                      ),
+                    ),
+                    child: Text(S.of(context).update),
+                  )
+                ],
+              ),
+              const SizedBox(width: 50),
+              Row(
+                children: [
+                  Expanded(
+                    child: LabelTextField(
+                      title: S.of(context).phoneNumber,
+                      isColumn: false,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      backgroundColor: Theme.of(context).cardColor,
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: CommonAppUIConfig.primaryRadiusBorder,
+                        side: BorderSide(
+                          color: Theme.of(context).dividerColor,
+                          width: 0.5,
+                        ),
+                      ),
+                    ),
+                    child: Text(S.of(context).update),
+                  )
+                ],
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
