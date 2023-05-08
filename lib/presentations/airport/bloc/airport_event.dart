a36c683e0e51c794d1b7e7fe06b2e2df25e59d7d
Nguyễn Minh Hưng <90996598+minhunsocute@users.noreply.github.com>
2023-05-08 18:01:17 +0700

    🤔Up some code

--- lib/presentations/airport/bloc/airport_event.dart
+++ lib/presentations/airport/bloc/airport_event.dart
@@ -5,4 +5,6 @@ class AirportEvent with _$AirportEvent {
   const factory AirportEvent.onStarted() = _Started;
   const factory AirportEvent.fetchAirports() = _FetchAirports;
   const factory AirportEvent.changePageView() = _ChangePageView;
+  const factory AirportEvent.openAddEditAirportForm(String id) =
+      _OpenAddEditAirportForm;
 }
