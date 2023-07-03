b4bf9c81b5d7051de13dff4efe0fb97e5b518478
Nguyễn Minh Hưng <90996598+minhunsocute@users.noreply.github.com>
2023-07-03 15:47:10 +0700

    🐳Update env

--- lib/core/components/configurations/env/env_prod.dart
+++ lib/core/components/configurations/env/env_prod.dart
@@ -1,9 +1,9 @@
 final Map<String, dynamic> environmentProd = {
   'name': 'Flight Booking',
   'environment': 'prod',
-  // 'baseUrl': 'http://localhost:8080/',
-  'baseUrl': 'https://flight-booking-390802.as.r.appspot.com/',
-  'mode': 'web',
+  'baseUrl': 'http://192.168.1.104:8080/',
+  // 'baseUrl': 'https://flight-booking-390802.as.r.appspot.com/',
+  'mode': 'mobile',
 };
 
 //'baseUrl': 'http://10.0.23.27:8080/',
